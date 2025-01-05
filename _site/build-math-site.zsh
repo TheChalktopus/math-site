463e2671d76745f869e03cc0606b038c1bc52da0
Abby Kinney <ak@ACK-MacBook-Pro.local>
2025-01-05 14:30:25 -0600

    building the site again

--- _site/build-math-site.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-emacs -Q --script ~/Sites/math-site/build-site.el
-cd ~/Sites/math-site/
-git pull origin main
-git add -A
-git commit -m "building the site again"
-git push origin main
